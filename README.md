70f0d726c6198397a5c63f6583b2c497546c4a0d
Molly Donegan <mollyrdonegan@gmail.com>
2023-01-20 09:50:01 -0800

    WIP

--- README.md
+++ README.md
@@ -1,20 +1,95 @@
 Describe: robogerInteger
 
-Test: "It should recognize the action integers (1, 3, 5)."
+<!-- Test: "It should recognize the action integers (1, 3, 5)."
 Code: 
     const input = 3
     robogerInteger(input)
-Expected Output: true
+Expected Output: true -->
 
-Describe: robogerIntegerId
+Describe: robogerIntegerId1
+
+Test: "It should recognize action integer 1"
+Code:
+    const input = 1
+    robogerIntegerId1(input)
+Expected output: true
+
+Describe: robogerIntegerId3
+
+Test: "It should recognize action integer 3"
+Code:
+    const input = 3
+    robogerIntegerId1(input)
+Expected output: true
+
+Describe: robogerIntegerId5
+
+Test: "It should recognize action integer 5"
+Code:
+    const input = 5
+    robogerIntegerId1(input)
+Expected output: true
+
+<!-- Describe: robogerIntegerId
 
 Test: "It should recognize all instances of an action integer in a number."
 Code:
     const input = 25
     robogerIntegerId(input)
+Expected output: false, true -->
+
+Describe: robogerId1
+
+Test: "It should recognize all instances of action integer 1 in a number."
+Code:
+    const input = 14
+    robogerIntegerId(input)
+Expected output: true, false
+
+Describe: robogerId3
+
+Test: "It should recognize all instances of action integer 3 in a number."
+Code:
+    const input = 23
+    robogerIntegerId(input)
 Expected output: false, true
 
-Describe
+Describe: robogerId5
+
+Test: "It should recognize all instances of an action integer 5 in a number."
+Code:
+    const input = 25
+    robogerIntegerId(input)
+Expected output: false, true
+
+
+
+
+Describe: robogerReplace1
+
+Test: "For numbers containing action integer 1, it should replace the entire number with 'Beep!'"
+Code:
+    const input = 14
+    robogerReplace1(input)
+Expected output: 'Beep!'
+
+Describe: robogerReplace3
+
+Test: "For numbers containing action integer 3, it should replace the entire number with 'Boop!'"
+Code:
+    const input = 23
+    robogerReplace3(input)
+Expected output: 'Boop!'
+
+Describe: robogerReplace5
+
+Test: "For numbers containing action integer 5, it should replace the entire number with 'Won't you be my neighbor?'"
+Code:
+    const input = 25
+    robogerReplace5(input)
+Expected output: "Won't you be my neighbor?"
+
+
 
 Describe: beepBoop()
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,136 @@
 //Utility
 
-function robogerInteger(input) {
+// function robogerInteger(input) {
+//     const integer = input.toString().split("");
+//     if (integer === "1" || integer === "3" || integer === "5") {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+function robogerIntegerId1(input) {
     const integer = input.toString();
-    if (integer === "1" || integer === "3" || integer === "5") {
+    if (integer === "1") {
         return true;
     } else {
         return false;
     }
 }
 
+function robogerIntegerId3(input) {
+    const integer = input.toString();
+    if (integer === "3") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function robogerIntegerId5(input) {
+    const integer = input.toString();
+    if (integer === "5") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
 //Business Logic
+
+// function robogerIntegerId(input) {
+//     const userInput = input.toString().split("");
+//     const inputArray = [];
+//     userInput.forEach(function (integer) {
+//         if (robogerInteger(integer)) {
+//             inputArray.push(true);
+//         } else {
+//             inputArray.push(false);
+//         }
+//     });
+//     return inputArray;
+// }
+
+function robogerId1(input) {
+    const userInput = input.toString().split("");
+    const inputArray = [];
+    userInput.forEach(function (integer) {
+        if (robogerIntegerId1(integer)) {
+            inputArray.push(true);
+        } else {
+            inputArray.push(false);
+        }
+    });
+    return inputArray;
+}
+
+function robogerId3(input) {
+    const userInput = input.toString().split("");
+    const inputArray = [];
+    userInput.forEach(function (integer) {
+        if (robogerIntegerId3(integer)) {
+            inputArray.push(true);
+        } else {
+            inputArray.push(false);
+        }
+    });
+    return inputArray;
+}
+
+function robogerId5(input) {
+    const userInput = input.toString().split("");
+    const inputArray = [];
+    userInput.forEach(function (integer) {
+        if (robogerIntegerId5(integer)) {
+            inputArray.push(true);
+        } else {
+            inputArray.push(false);
+        }
+    });
+    return inputArray;
+}
+
+
+
+// function robogerReplace(input) {
+//     const userInput = input.toString().split("");
+//     const inputArray = [];
+//     if (robogerIntegerId1)
+// }
+
+function oneWordPigLatin(text) {
+    let characterArray = text.toLowerCase().split("");
+    let newWord = [];
+    let newWord2;
+    if (isVowel(firstLetter(text))) {
+        newWord = appendAy(text);
+        return newWord;
+    } else if (isVowel(secondLetter(text))) {
+        newWord = characterArray.shift()
+        newWord2 = characterArray.join("");
+        newWord2 = newWord2 + appendAy(firstLetter(text));
+        return newWord2;
+    } else if (isVowel(thirdLetter(text))) {
+        newWord = characterArray.slice(2);
+        newWord = newWord.join("");
+        newWord2 = newWord + appendAy(firstLetter(text) + secondLetter(text));
+        return newWord2;
+    }
+}
+
+
+
+
+
+for (let i=1; i<=100; i+=1){
+    if (i === 1){
+        console.log("Beep!");
+    } else if (i === 3) {
+        console.log("Boop!");
+    } else if (i === 5) {
+        console.log("Won't you be my neighbor?");
+    } else {
+        console.log(i);
+    }
+}
