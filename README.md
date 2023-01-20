bafb8d532dc536ab8f5678935233b153404acc00
Molly Donegan <mollyrdonegan@gmail.com>
2023-01-20 13:11:42 -0800

    WIP

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-Describe: robogerInteger
+<!-- Describe: robogerInteger
 
-Test: "It should recognize the action integers (1, 3, 5)."
+Test: "It should recognize the action integers (1, 2, 3)."
 Code: 
     robogerInteger(3)
-Expected Output: true
+Expected Output: true -->
+
 
 Describe: robogerIntegerId1
 
@@ -12,6 +13,15 @@ Code:
     robogerIntegerId1(1)
 Expected output: true
 
+
+Describe: robogerIntegerId3
+
+Test: "It should recognize action integer 2"
+Code:
+    robogerIntegerId2(2)
+Expected output: true
+
+
 Describe: robogerIntegerId3
 
 Test: "It should recognize action integer 3"
@@ -19,71 +29,60 @@ Code:
     robogerIntegerId1(3)
 Expected output: true
 
-Describe: robogerIntegerId5
 
-Test: "It should recognize action integer 5"
-Code:
-    robogerIntegerId1(5)
-Expected output: true
-
-Describe: robogerIntegerId
+<!-- Describe: robogerIntegerId
 
 Test: "It should recognize all instances of an action integer in a number."
 Code:
     robogerIntegerId(25)
-Expected output: false, true
+Expected output: true, false -->
 
-Describe: robogerId1
+<!-- Describe: robogerId1
 
 Test: "It should recognize all instances of action integer 1 in a number."
 Code:
     robogerIntegerId(14)
 Expected output: true, false
 
+Describe: robogerId2
+
+Test: "It should recognize all instances of action integer 2 in a number."
+Code:
+    robogerIntegerId(24)
+Expected output: true, false
+
 Describe: robogerId3
 
-Test: "It should recognize all instances of action integer 3 in a number."
+Test: "It should recognize all instances of an action integer 3 in a number."
 Code:
-    robogerIntegerId(23)
-Expected output: false, true
-
-Describe: robogerId5
-
-Test: "It should recognize all instances of an action integer 5 in a number."
-Code:
-    robogerIntegerId(25)
-Expected output: false, true
-
-
+    robogerIntegerId(53)
+Expected output: false, true -->
 
 
 Describe: robogerId
 
-Test: "For numbers containing action integer 1, it should replace the entire number with 'Beep!'"
+Test: "For numbers containing an action integer 1, it should replace the entire number with 'Beep!'"
 Code:
     robogerId1(14)
 Expected output: 'Beep!'
 
-Describe: robogerReplace3
-
-Test: "For numbers containing action integer 3, it should replace the entire number with 'Boop!'"
-Code:
-    robogerId(23)
-Expected output: 'Boop!'
-
-Describe: robogerReplace5
-
-Test: "For numbers containing action integer 5, it should replace the entire number with 'Won't you be my neighbor?'"
+Test: "For numbers containing action integer 2, it should replace the entire number with 'Boop!'"
 Code:
     robogerId(25)
+Expected output: 'Boop!'
+
+Test: "For numbers containing action integer 3, it should replace the entire number with 'Won't you be my neighbor?'"
+Code:
+    robogerId(35)
 Expected output: "Won't you be my neighbor?"
 
 
 
+<!-- 
 Describe: beepBoop()
 
 Test: "It should return an array of numbers from 0 to the user's inputted number"
 Code: beepBoop(5);
 Expected Output: [0, 1, 2, 3, 4, 5]
 
-Test: "It should..." // this is the start of 2nd test for beepBoop()
+Test: "It should..." // this is the start of 2nd test for beepBoop() -->
--- js/scripts.js
+++ js/scripts.js
@@ -1,147 +1,151 @@
-//Utility
+window.onload = function () {
+    const form = document.querySelector("form#main-form");
 
-// function robogerInteger(input) {
-//     const integer = input.toString().split("");
-//     if (integer === "1" || integer === "3" || integer === "5") {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
 
-function robogerIntegerId1(input) {
-    const integer = input.toString();
-    if (integer === "1") {
-        return true;
-    } else {
-        return false;
-    }
-}
+        //Utility
 
-function robogerIntegerId3(input) {
-    const integer = input.toString();
-    if (integer === "3") {
-        return true;
-    } else {
-        return false;
-    }
-}
+        // function robogerInteger(input) {
+        //     const integer = input.toString().split("");
+        //     if (integer === "1" || integer === "3" || integer === "5") {
+        //         return true;
+        //     } else {
+        //         return false;
+        //     }
+        // }
 
-function robogerIntegerId5(input) {
-    const integer = input.toString();
-    if (integer === "5") {
-        return true;
-    } else {
-        return false;
-    }
-}
+        function robogerIntegerId1(input) {
+            const integer = input.toString();
+            if (integer === "1") {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        function robogerIntegerId2(input) {
+            const integer = input.toString();
+            if (integer === "2") {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        function robogerIntegerId3(input) {
+            const integer = input.toString();
+            if (integer === "3") {
+                return true;
+            } else {
+                return false;
+            }
+        }
 
 
-//Business Logic
+        //Business Logic
 
-// function robogerIntegerId(input) {
-//     const userInput = input.toString().split("");
-//     const inputArray = [];
-//     userInput.forEach(function (integer) {
-//         if (robogerInteger(integer)) {
-//             inputArray.push(true);
-//         } else {
-//             inputArray.push(false);
-//         }
-//     });
-//     return inputArray;
-// }
+        // function robogerIntegerId(input) {
+        //     const userInput = input.toString().split("");
+        //     const inputArray = [];
+        //     userInput.forEach(function (integer) {
+        //         if (robogerInteger(integer)) {
+        //             inputArray.push(true);
+        //         } else {
+        //             inputArray.push(false);
+        //         }
+        //     });
+        //     return inputArray;
+        // }
 
-function robogerId(input) {
-    const userInput = input.toString().split("");
-    let userOutput = [];
-    userInput.forEach(function (integer) {
-        if (robogerIntegerId1(integer)) {
-            userOutput.push("Beep!");
-        } else if (robogerIntegerId3(integer)) {
-            userOutput.push("Boop!");
-        } else if (robogerIntegerId5(integer)) {
-            userOutput.push("Won't you be my neighbor?");
-        } else {
+        function robogerId(input) {
+            const userInput = input.toString().split("");
+            let userOutput = [];
+            userInput.forEach(function (integer) {
+                if (robogerIntegerId1(integer)) {
+                    userOutput.push("Beep!");
+                }
+                if (robogerIntegerId2(integer)) {
+                    userOutput.push("Boop!");
+                }
+                if (robogerIntegerId3(integer)) {
+                    userOutput.push("Won't you be my neighbor?");
+                } else {
+                    return userOutput;
+                }
+            });
             return userOutput;
         }
+
+        function generateList() {
+            let num = document.getElementById("num").value;
+            let list = "";
+            for (var i = 0; i <= num; i++) {
+                list += i + " ";
+            }
+            document.getElementById("list").innerHTML = list;
+        }
+
+        // function robogerId3(input) {
+        //     const userInput = input.toString().split("");
+        //     const inputArray = [];
+        //     userInput.forEach(function (integer) {
+        //         if (robogerIntegerId3(integer)) {
+        //             inputArray.push(true);
+        //         } else {
+        //             inputArray.push(false);
+        //         }
+        //     });
+        //     return inputArray;
+        // }
+
+        // function robogerId5(input) {
+        //     const userInput = input.toString().split("");
+        //     const inputArray = [];
+        //     userInput.forEach(function (integer) {
+        //         if (robogerIntegerId5(integer)) {
+        //             inputArray.push(true);
+        //         } else {
+        //             inputArray.push(false);
+        //         }
+        //     });
+        //     return inputArray;
+        // }
+
+        function oneWordPigLatin(text) {
+            let characterArray = text.toLowerCase().split("");
+            let newWord = [];
+            let newWord2;
+            if (isVowel(firstLetter(text))) {
+                newWord = appendAy(text);
+                return newWord;
+            } else if (isVowel(secondLetter(text))) {
+                newWord = characterArray.shift()
+                newWord2 = characterArray.join("");
+                newWord2 = newWord2 + appendAy(firstLetter(text));
+                return newWord2;
+            } else if (isVowel(thirdLetter(text))) {
+                newWord = characterArray.slice(2);
+                newWord = newWord.join("");
+                newWord2 = newWord + appendAy(firstLetter(text) + secondLetter(text));
+                return newWord2;
+            }
+        }
+
+        for (let i = 1; i <= 100; i += 1) {
+            if (i === 1) {
+                console.log("Beep!");
+            } else if (i === 2) {
+                console.log("Boop!");
+            } else if (i === 3) {
+                console.log("Won't you be my neighbor?");
+            } else {
+                console.log(i);
+            }
+        }
+
+        // UI Logic
+
+        document.getElementById("output").innerText = result;
     });
-    return userOutput;
 }
-
-// function robogerId3(input) {
-//     const userInput = input.toString().split("");
-//     const inputArray = [];
-//     userInput.forEach(function (integer) {
-//         if (robogerIntegerId3(integer)) {
-//             inputArray.push(true);
-//         } else {
-//             inputArray.push(false);
-//         }
-//     });
-//     return inputArray;
-// }
-
-// function robogerId5(input) {
-//     const userInput = input.toString().split("");
-//     const inputArray = [];
-//     userInput.forEach(function (integer) {
-//         if (robogerIntegerId5(integer)) {
-//             inputArray.push(true);
-//         } else {
-//             inputArray.push(false);
-//         }
-//     });
-//     return inputArray;
-// }
-
-function oneWordPigLatin(text) {
-    let characterArray = text.toLowerCase().split("");
-    let newWord = [];
-    let newWord2;
-    if (isVowel(firstLetter(text))) {
-        newWord = appendAy(text);
-        return newWord;
-    } else if (isVowel(secondLetter(text))) {
-        newWord = characterArray.shift()
-        newWord2 = characterArray.join("");
-        newWord2 = newWord2 + appendAy(firstLetter(text));
-        return newWord2;
-    } else if (isVowel(thirdLetter(text))) {
-        newWord = characterArray.slice(2);
-        newWord = newWord.join("");
-        newWord2 = newWord + appendAy(firstLetter(text) + secondLetter(text));
-        return newWord2;
-    }
-}
-
-for (let i = 1; i <= 100; i += 1) {
-    if (i === 1) {
-        console.log("Beep!");
-    } else if (i === 3) {
-        console.log("Boop!");
-    } else if (i === 5) {
-        console.log("Won't you be my neighbor?");
-    } else {
-        console.log(i);
-    }
-}
-
-// UI Logic
-window.addEventListener("load", function () {
-    const form = document.getElementById("numberInput");
-    form.addEventListener("submit", myNeighbor);
-})
-
-
-function handleFormSubmission() {
-    event.preventDefault();
-    const userNumber = document.getElementById("usernumber-input-id").value;
-    const theNeighborhood = myNeighbors(userNumber);
-    document.getElementById("user-house-number").innerText = userNumber;
-    document.getElementById("final-neighborhood").innerText = theNeighborhood;
-}
-
-window.addEventListener("load", function () {
-    this.document.querySelector("form#robogers-neighborhood").addEventListener("submit", handleFormSubmission);
-});
